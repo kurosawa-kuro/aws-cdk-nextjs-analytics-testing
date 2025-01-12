dd9ba802d60f0cd35f4f9438b6d9ce45923f0fa2
Toshifumi Kurosawa <kuromailserver@gmail.com>
2025-01-13 00:50:10 +0900

    Enhance README.md with detailed setup instructions for AWS CDK, including installation commands and dependency management using pnpm.

--- README.md
+++ README.md
@@ -18,3 +18,26 @@ sudo chown -R wsl:wsl /home/wsl/app
 cd /home/wsl/app
 git clone git@github.com:kurosawa-kuro/aws-cdk-nextjs-analytics-testing.git .
 ```
+
+```
+sudo npm install -g aws-cdk
+
+```
+
+pnpm up -L
+
+pnpm add -D aws-cdk@latest
+
+```
+# npmのロックファイルとnode_modulesを削除
+rm -rf package-lock.json node_modules
+
+# pnpmの設定
+pnpm import
+
+# 依存関係のインストール
+pnpm install
+
+# CDK関連の依存関係を再インストール
+pnpm add -D aws-cdk
+```
---
+++ infrastructure/env-validator/test/env-validator.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EnvValidator from '../lib/env-validator-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/env-validator-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EnvValidator.EnvValidatorStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
